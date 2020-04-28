876951ca509f2e1ae01c92c2a09c6f50ad89757f
amar gohil <amar7gohil@gmail.com>
2020-04-28 10:52:49 +0530

    Update InsertUpdateDeleteOnPosition.java

--- array/InsertUpdateDeleteOnPosition.java
+++ array/InsertUpdateDeleteOnPosition.java
@@ -1,37 +1,40 @@
-
 package array;
 
 public class InsertUpdateDeleteOnPosition {
+	static int size = 0;
+
 	static int addAtPosition(int arr[], int pos, int value, int l) {
-		if (pos > l - 1)
+		if (pos > size)
 			return -1;
-		for (int i = l - 1; i > pos; i--) {
+		for (int i = size; i > pos; i--) {
 			arr[i] = arr[i - 1];
 		}
 		arr[pos] = value;
+		size++;
 		return 1;
 	}
 
 	static int deleteAtPosition(int arr[], int pos, int l) {
-		if (pos > l - 1)
+		if (pos > size)
 			return -1;
-		for (int i = 0; i < l - 1; i++) {
-			if (i < pos)
-				continue;
+		for (int i = pos; i < size; i++) {
 			arr[i] = arr[i + 1];
 		}
+		size--;
 		return 1;
 	}
 
 	public static void main(String[] args) {
 		int arr[] = new int[10];
-		for (int i = 0; i < 5; i++)
+		for (int i = 0; i < 5; i++) {
 			arr[i] = i + 1;
+			size++;
+		}
 		int pos = 0, value = 8;
 		addAtPosition(arr, pos, value, arr.length);
 		for (int i = 0; i < arr.length; i++)
 			System.out.print(arr[i] + ", ");
-		deleteAtPosition(arr, pos, arr.length);
+		deleteAtPosition(arr, 3, arr.length);
 		System.out.println("After Delete");
 		for (int i = 0; i < arr.length; i++)
 			System.out.print(arr[i] + " ");
