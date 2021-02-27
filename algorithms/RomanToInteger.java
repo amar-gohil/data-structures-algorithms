a94e04f6424fb4896c1ecce6d7a1143ed6631098
amar gohil <amar7gohil@gmail.com>
2021-02-27 14:32:35 +0530

    Update RomanToInteger.java

--- algorithms/RomanToInteger.java
+++ algorithms/RomanToInteger.java
@@ -57,6 +57,43 @@ public class RomanToInteger {
 		return sum;
 	}
 
+	static int value(char r) {
+		if (r == 'I')
+			return 1;
+		if (r == 'V')
+			return 5;
+		if (r == 'X')
+			return 10;
+		if (r == 'L')
+			return 50;
+		if (r == 'C')
+			return 100;
+		if (r == 'D')
+			return 500;
+		if (r == 'M')
+			return 1000;
+		return -1;
+	}
+
+	static int romanToIntBestWay(String s) {
+		int sum = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int s1 = value(s.charAt(i));
+			if (i + 1 < s.length()) {
+				int s2 = value(s.charAt(i + 1));
+				if (s1 >= s2) {
+					sum = sum + s1;
+				} else {
+					sum = sum + s2 - s1;
+					i++;
+				}
+			} else {
+				sum = sum + s1;
+			}
+		}
+		return sum;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(romanToInt("MCMXCIV"));
 	}
