e7a5526ca3e2f275abf42ddc40aa8d3af3537b04
amar gohil <amar7gohil@gmail.com>
2021-05-11 19:18:23 +0530

    Create ListNode.java

---
+++ linkedlist/ListNode.java
@@ -0,0 +1,9 @@
+package com.datastructure.linkedlist;
+
+public class ListNode {
+        int val;
+        com.datastructure.linkedlist.ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, com.datastructure.linkedlist.ListNode next) { this.val = val; this.next = next; }
+}
