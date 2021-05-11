2fdba15d360ed10cb0a0cd276711a49af23ccf98
amar gohil <amar7gohil@gmail.com>
2021-05-11 19:20:09 +0530

    Update ListNode.java

--- linkedlist/ListNode.java
+++ linkedlist/ListNode.java
@@ -2,8 +2,12 @@ package com.datastructure.linkedlist;
 
 public class ListNode {
         int val;
-        com.datastructure.linkedlist.ListNode next;
-        ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, com.datastructure.linkedlist.ListNode next) { this.val = val; this.next = next; }
+        ListNode next;
+        ListNode(int val) {
+                this.val = val;
+        }
+        ListNode(int val, ListNode next) {
+                this.val = val;
+                this.next = next;
+        }
 }
