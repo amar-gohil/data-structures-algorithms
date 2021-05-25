dbd34462019a5ea5b041f1eda798704b9619de88
amar gohil <amar7gohil@gmail.com>
2021-05-25 16:41:25 +0530

    Update RecursiveDFS.java

--- graph/RecursiveDFS.java
+++ graph/RecursiveDFS.java
@@ -1,1 +1,24 @@
 
+package com.datastructure.graph;
+
+import java.util.HashMap;
+
+public class RecursiveDFS {
+    static HashMap<Node,Boolean> visited= new HashMap<Node, Boolean>();
+
+    static void recursiveDfsTraversal(Node root){
+        visited.put(root,true);
+        System.out.print(root.val + " | ");
+        for (Node neighbor: root.neighbors) {
+            if(visited.get(neighbor) == null){
+                recursiveDfsTraversal(neighbor);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        Node root = graph.getGraph();
+        recursiveDfsTraversal(root);
+    }
+}
